c6d78b106f51fd8477eeaa62ff31cc6c64d99de5
Andrew-Morris <blatadude@gmail.com>
2018-12-09 13:34:25 -0700

    removed unused packages, added cors, added .nowignore

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import graphqlHTTP from 'koa-graphql'
 import Koa from 'koa'
 import Router from 'koa-router'
+import cors from '@koa/cors'
 import {executableSchema as schema} from './Genius'
 
 const port = parseInt(process.env.PORT || 3001)
@@ -9,6 +10,7 @@ const server = new Koa()
 const router = new Router()
 
 router.all('/graphql', graphqlHTTP({schema, graphiql: true, pretty: true}))
+server.use(cors())
 server.use(router.routes())
 server.listen(port, () => {
   console.log('listening internally on port ' + port)
